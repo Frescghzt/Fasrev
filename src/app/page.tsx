12af953447095305e47d6fec26be359a3dee2664
Frescghzt <frescghzt@gmail.com>
2025-04-15 01:06:54 -0500

    feat(searchbar): se agrgo la barra de navegacion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import Link from 'next/link';
+import SearchBar from '@/components/SearchBar';
 
 export default function HomePage() {
   return (
     <div>
-  
+      {/*buscador*/}
+      <div className='bg-[#171A21] text-white py-2 m-8 flex justify-around'>
+        <Link className='content-center' href="/">Tienda de monedas</Link>
+        <Link className='content-center' href="/">Reseñas</Link>
+        <Link className='content-center' href="/">Merch</Link>
+        <Link className='content-center' href="/">Eventos</Link>
+        <Link className='content-center' href="/">Amigos</Link>
+        <SearchBar />
+      </div>
     </div>
   );
 }
